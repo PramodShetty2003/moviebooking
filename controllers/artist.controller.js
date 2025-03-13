b2c83efe3983787ef787be42c08bbd8e3557f969
PramodShetty2003 <debuggingmymind@gmail.com>
2025-03-13 20:41:24 +0530

    Add artist.controller.js to implement findAllArtists functionalities

---
+++ controllers/artist.controller.js
@@ -0,0 +1,12 @@
+const Artist = require('../models/artist.model');
+
+const findAllArtists = async (req, res) => {
+    try {
+        const genres = await Artist.find();
+        res.status(200).json(genres);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+module.exports ={ findAllArtists };
