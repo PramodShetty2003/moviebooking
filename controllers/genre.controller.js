e54816383723272050ebe7d3c836ea6589eb6b40
PramodShetty2003 <debuggingmymind@gmail.com>
2025-03-16 13:11:25 +0530

    Updated the response method

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -3,8 +3,11 @@ const { Genre } = require('../models');
 
 const findAllGenres = async (req, res) => {
     try {
+        console.log(req.body);
+        
         const genres = await Genre.find();
-        res.status(200).json(genres);
+        console.log(genres);
+        res.status(200).json({"genres":genres});
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
     }
