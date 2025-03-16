da8b7be35641d4a93fd3731fac049b541e1bece4
PramodShetty2003 <debuggingmymind@gmail.com>
2025-03-16 20:29:30 +0530

    Added  api endpoint to logout by removing access to authorised users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,5 +6,7 @@ const userController = require('../controllers/user.controller');
 router.post('/auth/signup', userController.signUp);
 // POST: Log in a user
 router.post('/auth/login', userController.login);
+// POST: Logout the user
+router.post('/auth/logout', userController.logout);
 
 module.exports = router;
