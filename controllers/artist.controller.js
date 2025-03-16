8db10618c7e5deb640a7e8b5539d8c3fc6c9500f
PramodShetty2003 <debuggingmymind@gmail.com>
2025-03-16 13:13:01 +0530

    Corrected the import format

--- controllers/artist.controller.js
+++ controllers/artist.controller.js
@@ -2,8 +2,8 @@ const { Artist } = require('../models');
 
 const findAllArtists = async (req, res) => {
     try {
-        const genres = await Artist.find();
-        res.status(200).json(genres);
+        const Artists = await Artist.find();
+        res.status(200).json(Artists);
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
     }
