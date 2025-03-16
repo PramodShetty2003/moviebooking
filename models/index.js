f9fa3ab3d2e6737094b0b5e2a657d5b02a4419f2
PramodShetty2003 <debuggingmymind@gmail.com>
2025-03-16 20:30:31 +0530

    Added USer model to Centralised model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Genre = require('./genre.model');
 const { Artist } = require('./artist.model');
 const Movie = require('./movie.model');
+const User = require('./user.model');
 
 module.exports = {
     Genre,
     Artist,
-    Movie
+    Movie,
+    User
 };
