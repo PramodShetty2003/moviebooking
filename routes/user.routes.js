33ac0f49ee04d8b3dcfa39178fdbd2101ed55314
PramodShetty2003 <debuggingmymind@gmail.com>
2025-03-16 20:18:03 +0530

    Added  api endpoint to login by giving access to authorised users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/user.controller');
 
 // POST: Sign up a new user
 router.post('/auth/signup', userController.signUp);
+// POST: Log in a user
+router.post('/auth/login', userController.login);
 
 module.exports = router;
